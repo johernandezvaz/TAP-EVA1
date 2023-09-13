0d44ebf662894c1a4ddcebeef002d9ab0ccee13a
José de Jesús Hernández Vázquez <142260720+johernandezvaz@users.noreply.github.com>
2023-09-12 18:54:47 -0600

    Add files via upload

---
+++ EVA1_7_STACK/src/eva1_7_stack/EVA1_7_STACK.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_stack;
+
+/**
+ *
+ * @author Josep
+ */
+public class EVA1_7_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Inicia Main");
+        A();
+        System.out.println("FIN MAIN");
+    }
+    
+    public static void A(){
+        System.out.println("Inicia A");
+        B();
+        System.out.println("Fin B");
+    }
+    
+    public static void B(){
+        System.out.println("Inicia B");
+        System.out.println("Fin B");
+    }
+    
+}
